bbdca24396cc1def7deafec175b74a3f7aa38231
Diogo Silva <diogomfc@hotmail.com>
2024-02-15 19:31:40 -0300

    feat: Migrations Drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,4 +3,8 @@ export default {
   schema: "./src/db/schema/index.ts",
   out: "./drizzle",
   driver: "pg",
+  dbCredentials: {
+    connectionString: "postgresql://docker:docker@localhost:5432/pizzashop",
+  },
+
 } satisfies Config;
