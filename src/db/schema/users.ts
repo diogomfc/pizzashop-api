df13d542216aa22f658bab3d7dafe2918c0a4447
Diogo Silva <diogomfc@hotmail.com>
2024-02-15 19:09:50 -0300

    feat: Relationships on Drizzle

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -4,7 +4,7 @@ import {createId } from '@paralleldrive/cuid2'
 
 export const userRolesEnum = pgEnum("user_role", ["manager", "customer"]);
 
-export const user = pgTable("users", {
+export const users = pgTable("users", {
   id: text("id").$defaultFn(() => createId()).primaryKey(),
   name: text("name").notNull(),
   email: text("email").unique().notNull(),
@@ -13,3 +13,5 @@ export const user = pgTable("users", {
   createdAt: timestamp("created_at").notNull().defaultNow(),
   updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });
+
+
