86dfeb5f20705da1d8c8211a43bae127a4f1161b
Diogo Silva <diogomfc@hotmail.com>
2024-02-16 11:21:38 -0300

    feat(route): Create restaurant

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,33 @@
 import { Elysia } from 'elysia'
+import { db } from '../db/connection'
+import { restaurants, users } from '../db/schema'
 
-const app = new Elysia()
+// const app = new Elysia()
+
+// Defining a route
+const app = new Elysia().post('/restaurants', async ({ body, set }) => {
+  const { restaurantName, name, email, phone } = body as any
+
+  const [manager] = await db
+    .insert(users)
+    .values({
+      name,
+      email,
+      phone,
+      role: 'manager',
+    })
+    .returning({
+      id: users.id,
+    })
+
+  await db.insert(restaurants).values({
+    name: restaurantName,
+    description: '',
+    managerId: manager.id,
+  })
+
+  set.status = 204
+})
 
 app.get('/', () => {
   return 'Hello, Elysia!'
